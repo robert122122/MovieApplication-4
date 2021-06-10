d2e3801ddd6564db46e1fd6e7539926a5f7d7f96
Robert Padurariu <robert.padurariu@emanuel.ro>
2021-06-10 23:31:29 +0300

    adding files

---
+++ MovieApplication/Data/MovieApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MovieApplication.Models;
+
+namespace MovieApplication.Data
+{
+    public class MovieApplicationContext : DbContext
+    {
+        public MovieApplicationContext(DbContextOptions<MovieApplicationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MovieApplication.Models.Movie> Movie { get; set; }
+    }
+}
